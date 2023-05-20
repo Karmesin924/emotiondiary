36354b6a03a3b302f75b7a8f876734c0330bbfc3
Karmesin924 <jinhong0924@naver.com>
2023-05-20 14:45:14 +0900

    button header component

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,25 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const id = searchParams.get("id");
+    const mode = searchParams.get("mode");
+
+    console.log("id : ", id);
+    console.log("mode : ", mode);
+
     return (
         <div>
             <h1>Edit</h1>
             <p>이곳은 일기 수정 페이지입니다.</p>
+            <button onClick={()=>{setSearchParams({id : "0924",mode : "light"});}}>
+            Query String 변경
+            </button>
+            <button onClick={()=>{navigate("./home");}}>
+            Home으로 이동!
+            </button>
         </div>
     );
 }
